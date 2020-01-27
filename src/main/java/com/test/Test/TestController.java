07df372ce13d5b760528273419e1c4145c1b47f6
Ranjan Raj <ranjanraj.raj22@gmail.com>
2020-01-27 10:31:56 +0530

    add two numbers

--- src/main/java/com/test/Test/TestController.java
+++ src/main/java/com/test/Test/TestController.java
@@ -1,4 +1,24 @@
 package com.test.Test;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class TestController {
-}
+
+    @RequestMapping("/hello")
+    public String sayHello(@RequestParam(value = "name") String name) {
+        String[] arr = name.split(",");
+        int size = arr.length;
+        int[] num = new int[size];
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            num[i] = Integer.parseInt(arr[i]);
+            sum = sum + num[i];
+        }
+        return "Sum of the numbers is " + sum;
+    }
+    }
